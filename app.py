b815dcdbbe2f9b70f34fc29ec028965d486ad664
Qaris Pratama <qarisp@gmail.com>
2025-06-10 22:12:53 +0800

    Fixed earthquake layer

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ earthquake_response = earthquake_table.scan()
 earthquake_items = earthquake_response['Items']
 
 earthquake_df = pd.DataFrame(earthquake_items)
-earthquake_df = earthquake_df.sort_values(by='timestamp', ascending=True)
+earthquake_df['timestamp'] = pd.to_datetime(earthquake_df['timestamp'])
 
 st.sidebar.markdown("🔔 GEMPA TERKINI")
 latest_eq = earthquake_df.iloc[0]
